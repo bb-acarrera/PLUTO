8555c0015103b53ba6f5f77d46ebdda189a0074f
Paul Tokarchuk <ptokarchuk@uncharted.software>
2017-06-07 15:10:32 -0400

    Removed the Rules router which isn't used and could lead to future confusion.

--- src/server/router.js
+++ src/server/router.js
@@ -6,8 +6,6 @@ const ValidationRouter = require('./routes/validation');
 const RulesetRouter = require('./routes/rulesets');
 const LogsRouter = require('./routes/logs');
 
-// const RulesRouter = require('./routes/rules');	// Obsolete. I'm keeping this code though in case I need a new rules route.
-
 class Router {
 	constructor(config) {
 		this._router = express.Router();
@@ -15,12 +13,10 @@ class Router {
 		this.validationRouter = new ValidationRouter(config);
 		this.rulesetRouter = new RulesetRouter(config);
 		this.logsRouter = new LogsRouter(config);
-		// this.rulesRouter = new RulesRouter(config);
 
 		this._router.get('/validation', (req, res) => this.validationRouter.get(req, res) );
 		this._router.get('/rulesets/:id', (req, res) => this.rulesetRouter.get(req, res) );
 		this._router.get('/logs/:id', (req, res) => this.logsRouter.get(req, res) );
-		// this._router.get('/rules/:id', (req, res) => this.rulesRouter.get(req, res) );
 
 		this._router.patch('/rulesets/:id', (req, res) => this.rulesetRouter.patch(req, res) );
 	}
