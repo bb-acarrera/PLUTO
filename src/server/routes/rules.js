13e57e9205ff202b4614e2c04162510a3ffba998
Paul Tokarchuk <ptokarchuk@uncharted.software>
2017-06-12 10:15:07 -0400

    Added code that loads a config file for rule files. This config will eventually describe the properties and property UI but for now just shows contains the properties with default values.

--- src/server/routes/rules.js
+++ src/server/routes/rules.js
@@ -15,13 +15,25 @@ class RulesRouter extends BaseRouter {
 			const extension = path.extname(file);
 			if (extension && extension == ".js") {
 				const basename = path.basename(file, extension);
+				const configName = basename + "Config.json";
+				const configFile = path.resolve(this.config.validator.config.RulesDirectory, configName);
+				var config = undefined;
+				if (fs.existsSync(configFile)) {
+					const contents = fs.readFileSync(configFile, 'utf8');
+					try {
+						config = JSON.parse(contents);
+					}
+					catch (e) {
+						console.log(`Failed to load ${configName}. Attempt threw:\n${e}\n`);
+					}
+				}
 				rules.push({
 					id: basename,
 					type: 'rule',
 					attributes: {
 						name: basename,
 						filename: basename,
-						config: {}
+						config: config
 					}
 				});
 			}
