4ef19dfd391928829622420a41e4ef72d2a443e8
Luke Corrigall <lcorrigall@oculusinfo.com>
2017-07-13 17:04:01 -0400

    started adding database support

---
+++ database/initdb.d/initTables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE runs (
+    id serial PRIMARY KEY,
+    log_id integer,
+    ruleset_id integer,
+    inputfile varchar(512),
+    outputfile varchar(512),
+    finishtime timestamp
+);
+
+CREATE TABLE logs (
+    id serial PRIMARY KEY,
+    log json
+);
+
+CREATE TABLE rulesets (
+    id serial PRIMARY KEY,
+    name varchar(256),
+    version integer DEFAULT 0
+)
--- startPluto.sh
+++ startPluto.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+docker run -v /var/lib/postgresql/data -v $PWD/database/initdb.d:/docker-entrypoint-initdb.d  -p 6543:5432 -e POSTGRES_PASSWORD=password -e POSTGRES_USER=pluto -e POSTGRES_DB=pluto -d postgres:9.6-alpine
 docker run -v $PWD/config:/opt/PLUTO/config -p 3000:3000 -d pluto
