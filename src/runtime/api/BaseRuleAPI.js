ca8cf4bf88755f0bfe018663fdc8c0c099ce6089
Paul Tokarchuk <ptokarchuk@uncharted.software>
2017-07-18 12:50:01 -0400

    Updated the JSDocs to reflect the new methods.

--- src/runtime/api/BaseRuleAPI.js
+++ src/runtime/api/BaseRuleAPI.js
@@ -3,11 +3,8 @@ const EventEmitter = require('events');
 /**
  * This API class is used to describe the interface to rule operations. This base class can be used by rules that
  * do noy interact with the data, for example metadata rules.
- *
- * The class extends EventEmitter so that rules run asynchronously. When a rule completes it should post the static
- * {@link BaseRuleAPI.NEXT} value.
  */
-class BaseRuleAPI extends EventEmitter {
+class BaseRuleAPI {
 
 	/**
 	 * The base constructor. This simply sets <code>this.config</code> to the passed in configuration object. This config object
@@ -21,19 +18,28 @@ class BaseRuleAPI extends EventEmitter {
 	 * @param localConfig {object} the rule's configuration as defined in the ruleset file or a standalone config file.
 	 */
 	constructor(localConfig) {
-		super();
-
 		this.config = localConfig || {};
 	}
 
+	/**
+	 * All rules derived from {@link BaseRuleAPI}, {@link RuleAPI}, or {@link MetadataRuleAPI} must implement this method.
+	 * This method is the heart of the rule doing whatever is required.
+	 * @returns {Error} if the rule cannot perform it's actions at all, otherwise the return value is specific to the base
+	 * class of the rule. (MetadataRuleAPI rules should return nothing. RuleAPI methods should call a method, defined
+	 * in RuleAPI, to return a filename, stream, or object. The {@link RuleAPI} documentation describes these in more
+	 * detail.
+	 */
 	run() {
 		return new Error("run() not implemented.");
 	}
 
 	/**
-	 *
-	 * @param data
-	 * @returns {Promise}
+	 * This method takes input from the validator and calls the rule's <code>run()</code> method.
+	 * @param data input from the validator. This data should come in one of three objects. <code>\{ file : filename \}</code>
+	 * if the input is a filename, <code>\{ data : object \}</code> if the input is a JavaScript object of some unspecified
+	 * type (though usually a string), and <code>\{ stream : stream \}</code> if the input is a readable stream.
+	 * @returns {Promise} a promise that returns the results of the rules <code>run()</code> method. This will have
+	 * the same structure as the <code>data</code> parameter.
 	 * @private
 	 */
 	_run(data) {
--- src/runtime/api/RuleAPI.js
+++ src/runtime/api/RuleAPI.js
@@ -5,13 +5,24 @@ const BaseRuleAPI = require('./BaseRuleAPI');
 
 /*
  * A trivial class which takes a data array and streams it.
+ * @private
  */
-class MemoryReaderStream extends stream.Readable {
+class _MemoryReaderStream extends stream.Readable {
+	/**
+	 *
+	 * @param data
+	 * @private
+	 */
 	constructor(data) {
 		super();
 		this.data = Buffer.from(data);
 	}
 
+	/**
+	 *
+	 * @param size
+	 * @private
+	 */
 	_read(size) {
 		this.push(this.data);
 		this.push(null);
@@ -20,13 +31,26 @@ class MemoryReaderStream extends stream.Readable {
 
 /*
  * A trivial class which takes input from a stream and captures it in a buffer.
+ * @private
  */
-class MemoryWriterStream extends stream.Writable {
+class _MemoryWriterStream extends stream.Writable {
+	/**
+	 *
+	 * @param options
+	 * @private
+	 */
 	constructor(options) {
 		super(options);
 		this.buffer = Buffer.from(''); // empty
 	}
 
+	/**
+	 *
+	 * @param chunk
+	 * @param enc
+	 * @param cb
+	 * @private
+	 */
 	_write(chunk, enc, cb) {
 		// our memory store stores things in buffers
 		const buffer = (Buffer.isBuffer(chunk)) ?
@@ -36,11 +60,17 @@ class MemoryWriterStream extends stream.Writable {
 		// concat to the buffer already there
 		this.buffer = Buffer.concat([this.buffer, buffer]);
 
-		// console.log("MemoryWriterStream DEBUG: " + chunk.toString());
+		// console.log("_MemoryWriterStream DEBUG: " + chunk.toString());
 
 		cb(null);
 	}
 
+	/**
+	 *
+	 * @param encoding
+	 * @returns {string}
+	 * @private
+	 */
 	getData(encoding) {
 		return this.buffer.toString(encoding);
 	}
@@ -51,8 +81,11 @@ class MemoryWriterStream extends stream.Writable {
  * data can be passed to and from the rule. Multiple input and output methods can return true. This allows the
  * application to select the best option for connecting two rules together.
  *
- * The class extends EventEmitter so that rules run asynchronously. When a rule completes it should post the static
- * {@link RuleAPI.NEXT} value.
+ * Classes derived from this class must implement {@link BaseRuleAPI#run BaseRuleAPI.run()}. The implementation
+ * should use the methods in this class to get the input data to the rule and set the output results using the
+ * <code>as*()</code> methods. A rule may return a <code>{@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Promise|Promise}
+ * </code> and if so must ensure that the <code>resolve()</code>
+ * methods returns via one of the <code>as*()</code> methods.
  */
 class RuleAPI extends BaseRuleAPI {
 	/**
@@ -70,34 +103,102 @@ class RuleAPI extends BaseRuleAPI {
 		super(localConfig);
 	}
 
+	/**
+	 * Checks whether the rule was initiated with a filename. <code>run()</code> methods would call this so they can
+	 * choose to use the filename as opposed to converting the filename into another form such as a stream or data.
+	 * @returns {boolean} <code>true</code> if the rule was initialized with a filename and <code>false</code>
+	 * otherwise.
+	 */
 	hasFilename() {
 		return this._data && this._data.file;
 	}
 
+	/**
+	 * Checks whether the rule was initiated with a readable stream. <code>run()</code> methods would call this so they can
+	 * choose to use the stream as opposed to converting it into another form such as a file or data.
+	 * @returns {boolean} <code>true</code> if the rule was initialized with a stream and <code>false</code>
+	 * otherwise.
+	 */
 	hasStream() {
 		return this._data && this._data.stream && this._data.stream instanceof stream.Readable;
 	}
 
+	/**
+	 * Checks whether the rule was initiated with a data object (unspecified but likely a string). <code>run()</code>
+	 * methods would call this so they can
+	 * choose to use the object as opposed to converting it into another form such as a file or stream.
+	 * @returns {boolean} <code>true</code> if the rule was initialized with a data object and <code>false</code>
+	 * otherwise.
+	 */
 	hasObject() {
 		return this._data && this._data.data;
 	}
 
+	/**
+	 * <code>run()</code> methods call this when they are returning a filename. Generally it would be used as
+	 * <pre><code>return asFile(...);</code></pre>.
+	 * @param filename the name of the file to return.
+	 * @returns an object used by the validator to encapsulate the file return.
+	 */
 	asFile(filename) {
 		return { file : filename };
 	}
 
+	/**
+	 * <code>run()</code> methods call this when they are returning a data object. Generally it would be used as
+	 * <code>return asObject(...);</code>
+	 * @param data the data object to return.
+	 * @returns an object used by the validator to encapsulate the object return.
+	 */
 	asObject(data) {
 		return { data : data };
 	}
 
+	/**
+	 * <code>run()</code> methods call this when they are returning a readable stream. Generally it would be used as
+	 * <pre><code>return asStream(...);</code></pre>
+	 * @param stream the readable stream to return.
+	 * @returns an object used by the validator to encapsulate the stream return.
+	 */
 	asStream(stream) {
 		return { stream : stream };
 	}
 
+	/**
+	 * When a <code>run()</code> method cannot return a valid result is calls this method with a descriptive error message.
+	 * Generally it would be used as
+	 * <pre><code>return asError(...);</code></pre>
+	 * @param message the error message to return.
+	 * @returns {Error} an Error object encapsulating the error message.
+	 */
 	asError(message) {
 		return new Error(message);
 	}
 
+	/**
+	 * A <code>run()</code> implementation calls this when it wants to get the data as an object (usually a String).
+	 * If the rule was initialized with a data object that object is returned. If it was initialized with a filename
+	 * then the file is first loaded and then the retrieved data is returned. If however the rule was initialized with
+	 * a stream a little more work is required. In this case the stream must be read and this is done asynchronously
+	 * so a Promise is returned. When the promise is resolved the data will be available. Rules that require an
+	 * object should have a <code>run()</code> method that is something like this.
+	 * <pre><code>
+run() {
+	let input = this.object;
+
+	if (input instanceof Promise)
+		return input.then((data) => {
+			return this._runImpl(data);
+		});
+	else
+		return this._runImpl(input);
+}
+	 </code></pre>
+	 * (where this._runImpl(data) is the actual implementation of the rule taking a data object as an argument.)<br/>
+	 * Rules may return Promises. However that promise must resolve() to an actual result. It cannot resolve to another
+	 * Promise.
+	 * @returns {*}
+	 */
 	get object() {
 		if (this._object)
 			return this._object;
@@ -107,7 +208,7 @@ class RuleAPI extends BaseRuleAPI {
 		if (this.hasFilename())
 			this._object = this.config.validator.loadFile(this._data.file, this.config.encoding);
 		else if (this.hasStream()) {
-			const writer = new MemoryWriterStream();
+			const writer = new _MemoryWriterStream();
 			this._data.stream.pipe(writer);	// I'm presuming this is blocking. (The docs don't mention either way.)
 			this._object = writer.getData(this.config.encoding);
 
@@ -124,6 +225,30 @@ class RuleAPI extends BaseRuleAPI {
 		return this._object;
 	}
 
+	/**
+	 * A <code>run()</code> implementation calls this when it wants to get the data from a file.
+	 * If the rule was initialized with a filename it is returned. If it was initialized with a data object
+	 * then that data is first saved to a file and the name of the file is returned. If however the rule was initialized with
+	 * a stream a little more work is required. In this case the stream must be written to a file and this is done asynchronously
+	 * so a Promise is returned. When the Promise is resolved the file will be available. Rules that require a
+	 * file should have a <code>run()</code> method that is something like this.
+	 * <pre><code>
+run() {
+	let input = this.inputFile;
+
+	if (input instanceof Promise)
+		return input.then((filename) => {
+			return this._runImpl(filename);
+		});
+	else
+		return this._runImpl(input);
+}
+	 </code></pre>
+	 * (where <code>this._runImpl(data)</code> is the actual implementation of the rule taking a filename as an argument.)<br/>
+	 * Rules may return Promises. However that promise must resolve() to an actual result. It cannot resolve to another
+	 * Promise.
+	 * @returns {*}
+	 */
 	get inputFile() {
 		if (this._inputFile)
 			return this._inputFile;
@@ -152,12 +277,16 @@ class RuleAPI extends BaseRuleAPI {
 		return this._inputFile;
 	}
 
-	get outputFile() {
-		if (!this._outputFile)
-			this._outputFile = this.config.validator.getTempName();
-		return this._outputFile;
-	}
-
+	/**
+	 * A <code>run()</code> implementation calls this when it wants to get the data through a stream.
+	 * If the rule was initialized with a stream it is returned. If it was initialized with a data object
+	 * then that data is written into a stream which is returned. Finally, if the rule was initialized with
+	 * a file then a stream is opened up reading the file and that stream is returned. Unlike {@link RuleAPI#object|object}
+	 * and {@link RuleAPI#inputFile|inputFile} this property will never return a Promise it will only ever return a readable stream.<br/>
+	 * Rules may return Promises. However that promise must resolve() to an actual result. It cannot resolve to another
+	 * Promise.
+	 * @returns {*}
+	 */
 	get inputStream() {
 		if (this._inputStream)
 			return this._inputStream;
@@ -167,11 +296,26 @@ class RuleAPI extends BaseRuleAPI {
 		else if (this.hasFilename())
 			this._inputStream = fs.createReadStream(this._data.file);
 		else
-			this._inputStream = new MemoryReaderStream(this._data.data);
+			this._inputStream = new _MemoryReaderStream(this._data.data);
 
 		return this._inputStream;
 	}
 
+	/**
+	 * This method returns a unique filename in the temporary directory. This guarantees it is cleaned up when
+	 * the application exits. Rules can use this method to get a unique name to use when they output to a file.
+	 * @returns {String} the absolute path to a unique, temporary file.
+	 */
+	get outputFile() {
+		if (!this._outputFile)
+			this._outputFile = this.config.validator.getTempName();
+		return this._outputFile;
+	}
+
+	/**
+	 * This method returns a writable stream that rules that use streams can use to write into.
+	 * @returns {stream.PassThrough} a stream that rules can write into and which can be read by following rules.
+	 */
 	get outputStream() {
 		if (!this._outputStream)
 			this._outputStream = new stream.PassThrough();
