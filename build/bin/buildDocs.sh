c372693196a4d2c5986cc493dd22ab63d99b9698
Paul Tokarchuk <ptokarchuk@uncharted.software>
2017-07-19 06:36:59 -0400

    Checking in the current set of generated docs.

--- build/bin/buildDocs.sh
+++ build/bin/buildDocs.sh
@@ -4,12 +4,12 @@ ROOT=`dirname $0`/../..
 ROOT=`cd $ROOT; echo $PWD`
 SRC=$ROOT/src
 BIN=$ROOT/node_modules/.bin
-DOCS=$ROOT/public/docs
+DOCS=$ROOT/docs/generatedDocs
 API=$SRC/runtime/api
 VAL=$SRC/validator
 
 if ! [ -e "$DOCS" ]; then
-	mkdir "$DOCS"
+	mkdir -p "$DOCS"
 # else
 # Don't want to do this as it will delete the .md files. Instead 'docs' should be cleaned
 # up manually.
