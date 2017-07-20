05780e68e8510d1260a30025e0124cbc7988f5ea
Luke Corrigall <lcorrigall@oculusinfo.com>
2017-07-20 08:56:33 -0400

    moved dev db to a different port

--- src/start_dev_db.sh
+++ src/start_dev_db.sh
@@ -3,4 +3,6 @@
 echo Must be run from PLUTO root dir
 
 docker volume create devpgdata
-docker run -v devpgdata:/var/lib/postgresql/data -v $PWD/database/initdb.d:/docker-entrypoint-initdb.d  -p 6543:6543 -e POSTGRES_PASSWORD=password -e POSTGRES_USER=pluto -e POSTGRES_DB=pluto -d postgres:9.6-alpine
+docker run -v devpgdata:/var/lib/postgresql/data -v $PWD/database/initdb.d:/docker-entrypoint-initdb.d  -p 6543:5432 -e POSTGRES_PASSWORD=password -e POSTGRES_USER=pluto -e POSTGRES_DB=pluto --name dev_database -d postgres:9.6-alpine
+cd src/runtime/rulesets
+node ../../../test_config/rulesets/import.js -v ../configs/validatorConfig.json
