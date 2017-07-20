b9cf52de44af8239f11e46340e0a8aeeb25168dd
Luke Corrigall <lcorrigall@oculusinfo.com>
2017-07-20 08:57:04 -0400

    updated/added helper scripts

--- startPluto.sh
+++ startPluto.sh
@@ -2,6 +2,9 @@
 
 docker network create -d bridge --subnet 192.168.0.0/24 --gateway 192.168.0.1 plutonet
 docker volume create pgdata
-docker run -v pgdata:/var/lib/postgresql/data -v $PWD/database/initdb.d:/docker-entrypoint-initdb.d  -p 5432:5432 -e POSTGRES_PASSWORD=password -e POSTGRES_USER=pluto -e POSTGRES_DB=pluto --net=plutonet -d postgres:9.6-alpine
-docker run -v $PWD/test_config:/opt/PLUTO/config -p 3000:3000 --net=plutonet -d pluto:develop
+docker run -v pgdata:/var/lib/postgresql/data -v $PWD/database/initdb.d:/docker-entrypoint-initdb.d  -p 5432:5432 -e POSTGRES_PASSWORD=password -e POSTGRES_USER=pluto -e POSTGRES_DB=pluto --net=plutonet --name pluto_db -d postgres:9.6-alpine
+docker run -v $PWD/test_config:/opt/PLUTO/config -p 3000:3000 --net=plutonet --name pluto_ws -d pluto:develop
 
+cd test_config/rulesets
+./importer_setup.sh
+./run_importer.sh
---
+++ test_config/rulesets/run_importer.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo Must be run from the rulesets folder
+
+node import.js -v ../validatorConfig.json
