d3b2cdc031e8dcb88a6d4830fcac2e198fcb03ab
Paul Tokarchuk <ptokarchuk@uncharted.software>
2017-07-21 14:54:28 -0400

    Added MemoryStreamWriter to the test to be able to examine test results.

--- src/validator/tests/unit/test-DeleteColumn.js
+++ src/validator/tests/unit/test-DeleteColumn.js
@@ -1,11 +1,40 @@
 /**
  * Created by cgerber on 2017-07-21.
  */
+const stream = require('stream');
 
 const ErrorLogger = require("../../ErrorLogger");
 const RuleAPI = require("../../../runtime/api/RuleAPI");
 const DeleteColumn = require("../../../runtime/rules/DeleteColumn");
+
 /*
+ * A trivial class which takes input from a stream and captures it in a buffer.
+ */
+class MemoryWriterStream extends stream.Writable {
+	constructor(options) {
+		super(options);
+		this.buffer = Buffer.from(''); // empty
+	}
+
+	_write(chunk, enc, cb) {
+		// our memory store stores things in buffers
+		const buffer = (Buffer.isBuffer(chunk)) ?
+			chunk :  // already is Buffer use it
+			new Buffer(chunk, enc);  // string, convert
+
+		// concat to the buffer already there
+		this.buffer = Buffer.concat([this.buffer, buffer]);
+
+		// console.log("MemoryWriterStream DEBUG: " + chunk.toString());
+
+		cb(null);
+	}
+
+	getData(encoding) {
+		return this.buffer.toString(encoding);
+	}
+}
+
 QUnit.test( "DeleteColumn: Creation Test", function(assert){
    const logger = new ErrorLogger();
    const config = {
@@ -17,16 +46,20 @@ QUnit.test( "DeleteColumn: Creation Test", function(assert){
    const rule = new DeleteColumn(config);
    const done = assert.async();
    rule._run( { data: data }).then((result) => {
-        const dataVar = result.data;
-        const logResults = logger.getLog();
-        console.log(result);
-        assert.ok(result, "Created");
-        assert.equal(dataVar, "Column 1\nb", "Expected only column 1");
-        done();
+	   	assert.ok(result, "Created");
+	   	const logResults = logger.getLog();
+		const writer = new MemoryWriterStream();
+		writer.on('finish', () => {
+			const dataVar = writer.getData();
+			console.log("dataVar = " + dataVar);
+
+			assert.equal(dataVar, "Column 1\nb", "Expected only column 1");
+			done();
+		})
+	   	result.stream.pipe(writer);	// I'm presuming this is blocking. (The docs don't mention either way.)
     });
 
 });
-*/
 
 /*
 QUnit.test( "DeleteColumn: Column Delete Test", function(assert){
