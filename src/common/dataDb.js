3ee6c4d3fea0c5a262b6c7be824da8b413d456cc
Luke Corrigall <lcorrigall@oculusinfo.com>
2017-07-25 13:49:31 -0400

    fixed log getter

--- src/common/dataDb.js
+++ src/common/dataDb.js
@@ -38,7 +38,7 @@ class data {
     getLog(id) {
 
         return new Promise((resolve, reject) => {
-            this.db.query("SELECT log FROM runs where run_id = $1", [id])
+            this.db.query("SELECT log FROM runs where id = $1", [id])
                 .then((result) => {
 
                     if(result.rows.length > 0) {
