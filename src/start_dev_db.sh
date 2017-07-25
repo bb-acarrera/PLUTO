d0c5ab160f3ba179215c929cf1c3db5a2d1dbce4
Luke Corrigall <lcorrigall@oculusinfo.com>
2017-07-25 10:33:17 -0400

    Renamed rulesets/import.js to importRulesets.js to reduce confusion

--- src/start_dev_db.sh
+++ src/start_dev_db.sh
@@ -5,4 +5,4 @@ echo Must be run from PLUTO root dir
 docker volume create devpgdata
 docker run -v devpgdata:/var/lib/postgresql/data -v $PWD/database/initdb.d:/docker-entrypoint-initdb.d  -p 6543:5432 -e POSTGRES_PASSWORD=password -e POSTGRES_USER=pluto -e POSTGRES_DB=pluto --name dev_database -d postgres:9.6-alpine
 cd src/runtime/rulesets
-node ../../../test_config/rulesets/import.js -v ../configs/validatorConfig.json
+node ../../../test_config/rulesets/importRulesets.js -v ../configs/validatorConfig.json
--- test_config/rulesets/run_importer.sh
+++ test_config/rulesets/run_importer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo Must be run from the rulesets folder
-node import.js -h localhost -p 5432 -d pluto -U pluto -W password
+node importRulesets.js -h localhost -p 5432 -d pluto -U pluto -W password
