58392c57242d86b18a0d1f532f24b31ff41260b4
Luke Corrigall <lcorrigall@oculusinfo.com>
2017-08-16 09:57:32 -0400

    refactored add & clone

--- src/client/app/controllers/index.js
+++ src/client/app/controllers/index.js
@@ -1,58 +1,45 @@
 import Ember from 'ember';
 
+
+function addRuleset(controller, rulesetId, ruleset) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = () => {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 201) {
+      controller.get('target.router').refresh();
+      controller.transitionToRoute('editRuleset', rulesetId);
+    }
+    else if (xmlHttp.readyState == 4) {
+      alert(`Failed to create. Status = ${xmlHttp.status}`);
+    }
+  };
+
+  let theUrl = document.location.origin + "/rulesets/";
+  let theJSON = {
+    rulesetId: rulesetId,
+    ruleset: ruleset
+  };
+
+  xmlHttp.open("POST", theUrl, true); // true for asynchronous
+  xmlHttp.setRequestHeader("Content-Type", "application/json");
+  xmlHttp.send(JSON.stringify(theJSON));
+}
+
 export default Ember.Controller.extend({
   queryParams: [],
   actions: {
     addRuleset() {
       var rulesetId = prompt("Enter a name for the new ruleset", "");
 
-      var xmlHttp = new XMLHttpRequest();
-      xmlHttp.onreadystatechange = () => {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 201) {
-          this.transitionToRoute('editRuleset', rulesetId);
-        }
-        else if (xmlHttp.readyState == 4) {
-          alert(`Failed to create. Status = ${xmlHttp.status}`);
-        }
-      };
-
-      let theUrl = document.location.origin + "/rulesets/";
-      let theJSON = {
-        rulesetId: rulesetId
-      };
-
-      xmlHttp.open("POST", theUrl, true); // true for asynchronous
-      xmlHttp.setRequestHeader("Content-Type", "application/json");
-      xmlHttp.send(JSON.stringify(theJSON));
-
+      addRuleset(this, rulesetId);
     },
 
     cloneRuleset(ruleset) {
       var rulesetId = prompt("Enter a name for the copied ruleset", "");
 
-      var xmlHttp = new XMLHttpRequest();
-      xmlHttp.onreadystatechange = () => {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 201) {
-          this.get('target.router').refresh();
-          this.transitionToRoute('editRuleset', rulesetId);
-        }
-        else if (xmlHttp.readyState == 4) {
-          alert(`Failed to create. Status = ${xmlHttp.status}`);
-        }
-      };
-
-      var ruleset = ruleset.toJSON().rules;
+      var rulesetCopy = ruleset.toJSON().rules;
       ruleset.name = "Copy of " + ruleset.name;
 
-      let theUrl = document.location.origin + "/rulesets/";
-      let theJSON = {
-        rulesetId: rulesetId,
-        ruleset: ruleset
-      };
-
-      xmlHttp.open("POST", theUrl, true); // true for asynchronous
-      xmlHttp.setRequestHeader("Content-Type", "application/json");
-      xmlHttp.send(JSON.stringify(theJSON));
+      addRuleset(this, rulesetId, rulesetCopy);
     },
 
     deleteRuleset(ruleset, rulesets) {
