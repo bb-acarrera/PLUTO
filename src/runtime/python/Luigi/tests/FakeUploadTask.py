e5bef119ab13ede0fb451d9fec910f4ba1acdd45
Luke Corrigall <lcorrigall@oculusinfo.com>
2017-09-08 09:48:45 -0400

    added ruleset and source config to parameters

--- src/runtime/python/Luigi/tests/FakeUploadTask.py
+++ src/runtime/python/Luigi/tests/FakeUploadTask.py
@@ -3,10 +3,10 @@ import os
 
 class FakeDownloadTask(luigi.Task):
 
-	sourceFile = luigi.Parameter()
+    targetFile = luigi.Parameter()
 
 	def output(self):
 		'''
 		Return a luigi.localTarget() object pointing to the output
 		'''
-		return luigi.LocalTarget(os.path.abspath(self.sourceFile))
+		return luigi.LocalTarget(os.path.abspath(targetFile))
