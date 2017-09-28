a4a95792f6745dd91bf3ec39077cee285ff19a3a
Luke Corrigall <lcorrigall@oculusinfo.com>
2017-09-28 11:35:37 -0400

    Moved database loader to docker container

--- build/bin/build.sh
+++ build/bin/build.sh
@@ -8,10 +8,13 @@ CLIENT=$ROOT/src/client
 EMBER_DST=$DST/server/public
 SERVER_DST=$DST/server
 DEPLOY_DST=$DST/deploy
+DB_SRC=$ROOT/database
 DB_DST=$DEPLOY_DST/database
 CONFIG_DST=$DEPLOY_DST/sample_config
 CONFIG=$ROOT/test_config
 DEPLOY_SRC=$ROOT/deploy
+DBLOADER_DST=$DST/dbloader
+DBLOADER_SRC=$DB_SRC/dbloader
 
 rm -rf $DST
 
@@ -50,11 +53,12 @@ mkdir $DEPLOY_DST
 mkdir $DB_DST
 mkdir $DB_DST/initdb.d
 mkdir $CONFIG_DST
+mkdir $DBLOADER_DST
 
 cp $DEPLOY_SRC/* $DEPLOY_DST
 
 #copy database files
-cp $ROOT/database/initdb.d/* $DB_DST/initdb.d
+cp $DB_SRC/initdb.d/* $DB_DST/initdb.d
 
 #build/copy sample config
 mkdir $CONFIG_DST/results
@@ -71,3 +75,12 @@ for file in copy.py export.js import.js validatorConfig.json
 do
     cp $CONFIG/$file $CONFIG_DST
 done
+
+#copy the dbloader
+cp $DBLOADER_SRC/* $DBLOADER_DST
+
+echo "Build the server docker image"
+docker build -t pluto:develop -f $SERVER_DST/Dockerfile $DST/server
+
+echo "Build the data load docker image"
+docker build -t pluto_dbloader:develop -f $DBLOADER_DST/Dockerfile $DBLOADER_DST
--- startPluto.sh
+++ startPluto.sh
@@ -4,7 +4,5 @@ docker network create -d bridge --subnet 192.168.0.0/24 --gateway 192.168.0.1 pl
 docker volume create pgdata
 docker run -v pgdata:/var/lib/postgresql/data -v $PWD/database/initdb.d:/docker-entrypoint-initdb.d  -p 5432:5432 -e POSTGRES_PASSWORD=password -e POSTGRES_USER=pluto -e POSTGRES_DB=pluto --net=plutonet --name pluto_db -d postgres:9.6-alpine
 docker run -v $PWD/test_config:/opt/PLUTO/config -p 3000:3000 --net=plutonet --name pluto_ws -d pluto:develop
-
-cd test_config/rulesets
-sh ./run_importer.sh
+docker run -v $PWD/test_config:/opt/PLUTO/config --net=plutonet --rm -a stdout -a stderr pluto_dbloader:develop
 
