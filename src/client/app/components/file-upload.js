8cd7727db31f5b723b05f55dde77bc1acbe83f17
Luke Corrigall <lcorrigall@oculusinfo.com>
2017-10-02 15:55:19 -0400

    Added rudimentary ability to test saved ruleset

---
+++ src/client/app/components/file-upload.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+import EmberUploader from 'ember-uploader';
+
+export default EmberUploader.FileField.extend({
+  filesDidChange: function(files) {
+    const uploader = EmberUploader.Uploader.create({
+      url: this.get('url')
+    });
+
+    if (!Ember.isEmpty(files)) {
+      // this second argument is optional and can to be sent as extra data with the upload
+      uploader.upload(files[0], { ruleset: this.get('ruleset') });
+    }
+
+    this.$().val(null);
+  },
+  actions: {
+    upload: function() {
+      this.$().click();
+    }
+  }
+});
--- src/server/routes/processFile.js
+++ src/server/routes/processFile.js
@@ -6,6 +6,7 @@ const fs = require('fs-extra');
 const path = require("path");
 
 
+
 /*
 example request
 {
@@ -31,6 +32,9 @@ class ProcessFileRouter extends BaseRouter {
         console.log('got processFile request');
 
         let ruleset = req.body.ruleset;
+        let inputFile = req.body.input;
+        let outputFile = req.body.output;
+        let importConfig = req.body.import;
 
         if(!ruleset) {
             res.json({
@@ -39,39 +43,32 @@ class ProcessFileRouter extends BaseRouter {
             return;
         }
 
-        //if(!ruleset.endsWith('.json')) {
-        //    ruleset = ruleset + '.json';
-        //}
+        this.processFile(ruleset, importConfig, inputFile, outputFile, next, res);
+    }
 
-        let overrideFile = null;
-
-
+    processFile(ruleset, importConfig, inputFile, outputFile, next, res, finishedFn) {
         new Promise((resolve) => {
-            if(req.body.import) {
+
+            var cmd = 'node validator/startValidator.js -r ' + ruleset + ' -c "' + this.config.validatorConfigPath + '"';
+            let overrideFile = null;
+
+            if (importConfig) {
                 overrideFile = this.getTempName(this.config) + '.json';
-                fs.writeFileSync(overrideFile, JSON.stringify({ import: req.body.import }), 'utf-8');
+                fs.writeFileSync(overrideFile, JSON.stringify({import: importConfig}), 'utf-8');
+                cmd += ' -v "' + overrideFile + '"';
+            } else {
+                cmd += ' -i "' + inputFile + '" -o "' + outputFile + '"';
             }
 
             const options = {
                 cwd: path.resolve('.')
             };
 
-
-            var cmd;
-
-              cmd = 'node validator/startValidator.js -r ' + ruleset + ' -c "' + this.config.validatorConfigPath + '"';
-
-            if(overrideFile) {
-              cmd += ' -v "' + overrideFile + '"';
-            } else {
-                cmd += ' -i "' +  req.body.input +  '" -o "' + req.body.output + '"';
-            }
-
             console.log('exec cmd: ' + cmd);
 
             child_process.exec(cmd, options, (error, stdout, stderr) => {
 
-                if(overrideFile) {
+                if (overrideFile) {
                     fs.unlink(overrideFile);
                 }
 
@@ -82,10 +79,14 @@ class ProcessFileRouter extends BaseRouter {
                 }
                 console.log(`stdout: ${stdout}`);
                 console.log(`stderr: ${stderr}`);
+
+                if(finishedFn) {
+                    finishedFn();
+                }
             });
 
             res.json({
-                processing: 'started ' + req.body.ruleset
+                processing: 'started ' + ruleset
             });
 
             resolve();
@@ -94,8 +95,43 @@ class ProcessFileRouter extends BaseRouter {
                 processing: 'Failed to start: ' + e
             });
         }).then(() => {
-            //do cleanup
+
         });
+    }
+
+    processUpload(req, res, next) {
+        if (!req.files)
+            return res.status(400).send('No files were uploaded.');
+
+        // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+        let file = req.files.file;
+
+        let fileToProcess = this.getTempName(this.config);
+        let ruleset = req.body.ruleset;
+        let outputFile = this.getTempName(this.config);
+
+        if(!ruleset) {
+            res.json({
+                processing: 'Failed to start: no rule specified'
+            });
+            return;
+        }
+
+        // Use the mv() method to place the file somewhere on your server
+        file.mv(fileToProcess, err => {
+            if (err)
+                return res.status(500).send(err);
+
+            this.processFile(ruleset, null, fileToProcess, outputFile, next, res, () => {
+                fs.unlink(outputFile);
+            });
+
+            fs.unlink(fileToProcess);
+
+
+        });
+
+
     }
 
     // Create a unique temporary filename in the temp directory.
