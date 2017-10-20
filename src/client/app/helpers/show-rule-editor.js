87befff0a09370b7d6bd468099b28f2b9afd61a2
Luke Corrigall <lcorrigall@oculusinfo.com>
2017-10-20 13:56:30 -0400

    cleaned up spacing

--- src/client/app/helpers/show-rule-editor.js
+++ src/client/app/helpers/show-rule-editor.js
@@ -1,106 +1,106 @@
 import Ember from 'ember';
 
 export function showRuleEditor(params, {ruleInstance, rules, ruleset, parsers, importers, exporters, rulesetconfiguis}) {
-	var content = "<div>";
+  var content = "<div>";
 
-	if (!ruleInstance || !rules)
-		return "";
-
-
-	var uiConfig;
-
-	const itemSets = [rules, parsers, importers, exporters, rulesetconfiguis];
-	let items;
-
-	for(var i = 0; i < itemSets.length; i++) {
-		items = itemSets[i];
-
-		items.forEach(item => {
-			if (item.get("filename") == ruleInstance.filename)
-				uiConfig = item.get("ui");
-		});
-
-		if(uiConfig) {
-			break;
-		}
-	}
-
-	//get the column names from the parser
-	var parser = ruleset.get("parser");
-
-	var columnLabels = [];
-	if(parser && parser.config && parser.config.columnNames) {
-		columnLabels = parser.config.columnNames;
-	}
-
-	let name = ruleInstance.name || ruleInstance.filename;
-	if(name) {
-		content += `<div>Name <input id="name" type="text" value="${name}"/></div><br/>`;
-	}
+  if (!ruleInstance || !rules)
+    return "";
 
 
-	if (ruleInstance && uiConfig && uiConfig.properties) {
-		// Place the name prompt at the top, always.
+  var uiConfig;
 
-		content += addProperties(ruleInstance, uiConfig.properties, columnLabels);
-	}
-	else if (ruleInstance && ruleInstance.config) {
-		// If there is no UI do the best we can.
-		// Place the name prompt at the top, always.
-		for (var key in ruleInstance.config) {
-			// if (key != 'name')
-			content += `<div>${key} <input id="${key}" type="text" value="${ruleInstance.config[key]}"/></div>`;
-		}
-	}
-	content += "</div>";
+  const itemSets = [rules, parsers, importers, exporters, rulesetconfiguis];
+  let items;
 
-	return Ember.String.htmlSafe(content);
+  for(var i = 0; i < itemSets.length; i++) {
+    items = itemSets[i];
+
+    items.forEach(item => {
+      if (item.get("filename") == ruleInstance.filename)
+        uiConfig = item.get("ui");
+    });
+
+    if(uiConfig) {
+      break;
+    }
+  }
+
+  //get the column names from the parser
+  var parser = ruleset.get("parser");
+
+  var columnLabels = [];
+  if(parser && parser.config && parser.config.columnNames) {
+    columnLabels = parser.config.columnNames;
+  }
+
+  let name = ruleInstance.name || ruleInstance.filename;
+  if(name) {
+    content += `<div>Name <input id="name" type="text" value="${name}"/></div><br/>`;
+  }
+
+
+  if (ruleInstance && uiConfig && uiConfig.properties) {
+    // Place the name prompt at the top, always.
+
+    content += addProperties(ruleInstance, uiConfig.properties, columnLabels);
+  }
+  else if (ruleInstance && ruleInstance.config) {
+    // If there is no UI do the best we can.
+    // Place the name prompt at the top, always.
+   for (var key in ruleInstance.config) {
+      // if (key != 'name')
+        content += `<div>${key} <input id="${key}" type="text" value="${ruleInstance.config[key]}"/></div>`;
+    }
+  }
+  content += "</div>";
+
+  return Ember.String.htmlSafe(content);
 }
 
 function addProperties(instance, properties, columnLabels) {
-	var content = "";
-	for (var i = 0; i < properties.length; i++) {
-		let property = properties[i];
-		if (!property.name)
-			continue;
+  var content = "";
+  for (var i = 0; i < properties.length; i++) {
+    let property = properties[i];
+    if (!property.name)
+      continue;
 
-		switch (property.type) {
-			case 'boolean':
-				content += addBooleanProperty(instance, property);
-				break;
-			case 'choice':
-				content += addChoiceProperty(instance, property);
-				break;
-			case 'column':
-				content += addColumnProperty(instance, property, columnLabels);
-				break;
-			case 'date':
-				content += addDateProperty(instance, property);
-				break;
-			case 'float':
-			case 'number':
-				content += addFloatProperty(instance, property);
-				break;
-			case 'integer':
-				content += addIntegerProperty(instance, property);
-				break;
-			case 'time':
-				content += addTimeProperty(instance, property);
-				break;
-			case 'validator':
-				content += addValidator(instance, property);
-				break;
-			case 'string': // Fall through to default.
-			default:
-				content += addStringProperty(instance, property);
-		}
-	}
-	return content;
+    switch (property.type) {
+      case 'boolean':
+        content += addBooleanProperty(instance, property);
+        break;
+      case 'choice':
+        content += addChoiceProperty(instance, property);
+        break;
+      case 'column':
+        content += addColumnProperty(instance, property, columnLabels);
+        break;
+      case 'date':
+        content += addDateProperty(instance, property);
+        break;
+      case 'float':
+      case 'number':
+        content += addFloatProperty(instance, property);
+        break;
+      case 'integer':
+        content += addIntegerProperty(instance, property);
+        break;
+      case 'time':
+        content += addTimeProperty(instance, property);
+        break;
+      case 'validator':
+        content += addValidator(instance, property);
+        break;
+      case 'string': // Fall through to default.
+      default:
+        content += addStringProperty(instance, property);
+    }
+  }
+  return content;
 }
 
 function addBooleanProperty(instance, property) {
-	let initialValue = instance.config[property.name] || property.default || false;
-	return `<div>${property.label || property.name} <input id="${property.name}" type="checkbox" ${initialValue ? "checked" : ""}/></div>`;
+  let initialValue = instance.config[property.name] || property.default || false;
+  return `<div>${property.label || property.name} <input id="${property.name}" type="checkbox" ${initialValue ? "checked" : ""}/></div>`;
 }
 
 function iterateChoices(choices, fn) {
@@ -145,79 +145,79 @@ function addChoiceProperty(instance, property) {
 }
 
 function addColumnProperty(instance, property, columnLabels) {
-	if (!columnLabels || columnLabels.length == 0)
-		return "";
+  if (!columnLabels || columnLabels.length == 0)
+    return "";
 
-	let content=`<div>${property.label || property.name} <select id="${property.name}">`
-	let initialValue = instance.config[property.name] || property.default || columnLabels[0];
-	for (var i = 0; i < columnLabels.length; i++) {
-		let choice = columnLabels[i];
-		content += `<option value="${choice}"`;
-		if (choice == initialValue)
-			content += " selected";
-		content += `>${choice}</option>`;
-	}
-	content += "</select></div>";
-	return content;
+  let content=`<div>${property.label || property.name} <select id="${property.name}">`
+  let initialValue = instance.config[property.name] || property.default || columnLabels[0];
+  for (var i = 0; i < columnLabels.length; i++) {
+    let choice = columnLabels[i];
+    content += `<option value="${choice}"`;
+    if (choice == initialValue)
+      content += " selected";
+    content += `>${choice}</option>`;
+  }
+  content += "</select></div>";
+  return content;
 }
 
 function addDateProperty(instance, property) {
-	var today = new Date();
-	let initialValue = instance.config[property.name] || property.default || property.minimum || property.maximum || (today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate());
-	let content = `<div>${property.label || property.name} <input id="${property.name}" type="date" value="${initialValue}"`;
-	if (property.minimum)
-		content += ` min="${property.minimum}"`;
-	if (property.maximum)
-		content += ` max="${property.maximum}"`;
-	content += `/></div>`;
-	return content;
+  var today = new Date();
+  let initialValue = instance.config[property.name] || property.default || property.minimum || property.maximum || (today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate());
+  let content = `<div>${property.label || property.name} <input id="${property.name}" type="date" value="${initialValue}"`;
+  if (property.minimum)
+    content += ` min="${property.minimum}"`;
+  if (property.maximum)
+    content += ` max="${property.maximum}"`;
+  content += `/></div>`;
+  return content;
 
 }
 
 function addFloatProperty(instance, property) {
-	let initialValue = instance.config[property.name] || property.default || property.minimum || property.maximum || 0;
-	let content = `<div>${property.label || property.name} <input id="${property.name}" type="number" value="${initialValue}" step="0.01"`;  // TODO: Calculate a better step value?
-	if (property.minimum)
-		content += ` min="${property.minimum}"`;
-	if (property.maximum)
-		content += ` max="${property.maximum}"`;
-	content += `/></div>`;
-	return content;
+  let initialValue = instance.config[property.name] || property.default || property.minimum || property.maximum || 0;
+  let content = `<div>${property.label || property.name} <input id="${property.name}" type="number" value="${initialValue}" step="0.01"`;  // TODO: Calculate a better step value?
+  if (property.minimum)
+    content += ` min="${property.minimum}"`;
+  if (property.maximum)
+    content += ` max="${property.maximum}"`;
+  content += `/></div>`;
+  return content;
 }
 
 function addIntegerProperty(instance, property) {
-	let initialValue = instance.config[property.name] || property.default || property.minimum || property.maximum || 0;
-	let content = `<div>${property.label || property.name} <input id="${property.name}" type="number" value="${initialValue}"`;
-	if (property.minimum)
-		content += ` min="${property.minimum}"`;
-	if (property.maximum)
-		content += ` max="${property.maximum}"`;
-	content += `/></div>`;
-	return content;
+  let initialValue = instance.config[property.name] || property.default || property.minimum || property.maximum || 0;
+  let content = `<div>${property.label || property.name} <input id="${property.name}" type="number" value="${initialValue}"`;
+  if (property.minimum)
+    content += ` min="${property.minimum}"`;
+  if (property.maximum)
+    content += ` max="${property.maximum}"`;
+  content += `/></div>`;
+  return content;
 }
 
 function addStringProperty(instance, property) {
-	let initialValue = instance.config[property.name] || property.default || "";
-	return `<div>${property.label || property.name} <input id="${property.name}" type="text" value="${initialValue}"/></div>`;
+  let initialValue = instance.config[property.name] || property.default || "";
+  return `<div>${property.label || property.name} <input id="${property.name}" type="text" value="${initialValue}"/></div>`;
 }
 
 function addTimeProperty(instance, property) {
-	var today = new Date();
-	let initialValue = instance.config[property.name] || property.default || property.minimum || property.maximum || (today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds());
-	let content = `<div>${property.label || property.name} <input id="${property.name}" type="time" value="${initialValue}"`;
-	if (property.minimum)
-		content += ` min="${property.minimum}"`;
-	if (property.maximum)
-		content += ` max="${property.maximum}"`;
-	content += `/></div>`;
-	return content;
+  var today = new Date();
+  let initialValue = instance.config[property.name] || property.default || property.minimum || property.maximum || (today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds());
+  let content = `<div>${property.label || property.name} <input id="${property.name}" type="time" value="${initialValue}"`;
+  if (property.minimum)
+    content += ` min="${property.minimum}"`;
+  if (property.maximum)
+    content += ` max="${property.maximum}"`;
+  content += `/></div>`;
+  return content;
 
 }
 
 function addValidator(instance, property) {
-	let initialValue = instance.config[property.name] || property.default || "";
-	let validator = property.validator || "*";
-	return `<div>${property.label || property.name} <input id="${property.name}" type="text" pattern="${validator}" value="${initialValue}"/></div>`;
+  let initialValue = instance.config[property.name] || property.default || "";
+  let validator = property.validator || "*";
+  return `<div>${property.label || property.name} <input id="${property.name}" type="text" pattern="${validator}" value="${initialValue}"/></div>`;
 }
 
 export default Ember.Helper.helper(showRuleEditor);
