ae7783790db7f247d6f0d201eafe6ea4bb83ebba
Luke Corrigall <lcorrigall@oculusinfo.com>
2017-10-20 17:18:15 -0400

    Used validator config to set db connection properties in startup scripts

--- deploy/initPluto.sh
+++ deploy/initPluto.sh
@@ -2,6 +2,24 @@
 
 echo This must be run from the root pluto folder where this shell script resides
 
+if hash node 2>/dev/null; then
+    cd sample_config
+
+    POSTGRES_PORT=$(node -pe "require('./validatorConfig.json').dbPort")
+    POSTGRES_USER=$(node -pe "require('./validatorConfig.json').dbUser")
+    POSTGRES_PASSWORD=$(node -pe "require('./validatorConfig.json').dbPassword")
+    POSTGRES_DATABASE=$(node -pe "require('./validatorConfig.json').dbDatabase")
+
+    cd ..
+else
+    POSTGRES_PORT=5432
+    POSTGRES_USER=pluto
+    POSTGRES_PASSWORD=password
+    POSTGRES_DATABASE=pluto
+fi
+
+
+
 docker stop pluto_server
 docker rm pluto_server
 
@@ -12,7 +30,7 @@ docker network create -d bridge --subnet 192.168.0.0/24 --gateway 192.168.0.1 pl
 docker volume create pgdata
 
 #start the database container, if the pluto database doesn't exist, create it and run all the .sql files in database/init.d (which will create the tables)
-docker run -v pgdata:/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_PASSWORD=password -e POSTGRES_USER=pluto -e POSTGRES_DB=pluto --net=plutonet --name pluto_db -d postgres:9.6-alpine
+docker run -v pgdata:/var/lib/postgresql/data -p $POSTGRES_PORT:5432 -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_DB=$POSTGRES_DATABASE --net=plutonet --name pluto_db -d postgres:9.6-alpine
 
 #configure the database
 docker run -v $PWD/sample_config:/opt/PLUTO/config --net=plutonet --rm -a stdout -a stderr pluto_dbloader
