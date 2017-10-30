846317a2c777079ca8165d8c2e5f5bef8e330ad1
Luke Corrigall <lcorrigall@oculusinfo.com>
2017-10-30 15:57:58 -0400

    Fixed other failing tests

--- src/validator/tests/testRules/RuleErrorFromColumn.js
+++ src/validator/tests/testRules/RuleErrorFromColumn.js
@@ -25,15 +25,15 @@ class TestRulePassFail extends TableRuleAPI {
 			const type = record[this.column].trim();
 
 			if(type === ErrorHandlerAPI.ERROR) {
-				this.error(`Row ${rowId} has error`);
+				this.error(`Row ${rowId} has error`, rowId);
 			}
 
 			if(type === ErrorHandlerAPI.WARNING) {
-				this.warning(`Row ${rowId} has warning`);
+				this.warning(`Row ${rowId} has warning`, rowId);
 			}
 
 			if(type === ErrorHandlerAPI.INFO) {
-				this.info(`Row ${rowId} has info`);
+				this.info(`Row ${rowId} has info`, rowId);
 			}
 		}
 
--- src/validator/tests/testRules/RulePassFail.js
+++ src/validator/tests/testRules/RulePassFail.js
@@ -23,15 +23,15 @@ class TestRulePassFail extends TableRuleAPI {
 
 		if(this.config.rows) {
 			if(this.config.rows[rowId] === ErrorHandlerAPI.ERROR) {
-				this.error(`Row ${rowId} has error`);
+				this.error(`Row ${rowId} has error`, rowId);
 			}
 
 			if(this.config.rows[rowId] === ErrorHandlerAPI.WARNING) {
-				this.warning(`Row ${rowId} has warning`);
+				this.warning(`Row ${rowId} has warning`, rowId);
 			}
 
 			if(this.config.rows[rowId] === ErrorHandlerAPI.INFO) {
-				this.info(`Row ${rowId} has info`);
+				this.info(`Row ${rowId} has info`, rowId);
 			}
 		}
 
