05999be071aa67c21579bf721952a5435bbc8f8b
Paul Tokarchuk <ptokarchuk@uncharted.software>
2017-11-01 09:17:01 -0400

    Got sockets working to communicate config information between the client and server.

--- src/rules/RunPythonScript.js
+++ src/rules/RunPythonScript.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra');
 const net = require('net');
 const path = require("path");
-const spawnSync = require('child_process').spawnSync;
+const spawn = require('child_process').spawn;
 
 const OperatorAPI = require("../api/RuleAPI");
 
@@ -47,7 +47,14 @@ class RunPythonScript extends OperatorAPI {
 				c.on('end', () => {
 					console.log('client disconnected');
 				});
-				c.write(JSON.stringify(this.config));
+				
+				c.write(JSON.stringify(this.config, (key, value) => {
+					if (key == 'validator')
+						return null;
+					else
+						return value;
+				}));
+				
 	//			c.pipe(c);	Can't find documentation on what this does and the code works without it.
 				c.end();
 				server.unref();
@@ -60,43 +67,46 @@ class RunPythonScript extends OperatorAPI {
 			});
 		}
 	
-		let scriptName = path.basename(this.config.pythonScript);		// Why is the script's path stripped off?
-		try {
+		let scriptName = path.basename(this.config.pythonScript);	
+		return new Promise((resolve, reject) => {
 			// Run the python script. This complains if the script doesn't exist.
-			const results = spawnSync('python', [pythonScript, inputName, outputName, this.config.encoding || 'utf8', this.socketName]);
-
-			if (results) {
-				// Write any stdout/stderr output to the error log.
-
-				if (typeof results.stdout === 'string')
-					this.warning(results.stdout);
-				else if (results.stdout && typeof results.stdout.toString === 'function') {
-					let str = results.stdout.toString();
+			const python = spawn('python', [pythonScript, inputName, outputName, this.config.encoding || 'utf8', this.socketName]);
+			python.stdout.on('data', (data) => {
+				if (typeof data === 'string')
+					this.warning(data);
+				else if (data && typeof data.toString === 'function') {
+					let str = data.toString();
 					let strs = str.split("\n");
 					for (var i = 0; i < strs.length; i++) {
 						if (strs[i].length > 0)
 							this.warning(`${scriptName} wrote to stdout: ${strs[i]}.`);
 					}
 				}
-
-				if (typeof results.stderr === 'string')
-					this.error(results.stderr);
-				else if (results.stderr && typeof results.stderr.toString === 'function') {
-					let str = results.stderr.toString().trim();
+			});
+			
+			python.stderr.on('data', (data) => {
+				if (typeof data === 'string')
+					this.error(data);
+				else if (data && typeof data.toString === 'function') {
+					let str = data.toString().trim();
 					let strs = str.split("\n");
 					for (var i = 0; i < strs.length; i++)
 						if (strs[i].length > 0)
 							this.error(`${scriptName} wrote to stderr: ${strs[i]}.`);
 				}
-
-				if (results.status != 0)
-					this.error(`${scriptName} exited with status ${results.status}.`);
-			}
-		} catch (e) {
-			this.error(`${scriptName}: ${e}`);
-		}
-
-		return outputName;
+			});
+			
+			python.on('error', (err) => {
+	            this.error(`${scriptName}: Launching script failed with error: ${err}`);
+			});
+			
+			python.on('close', (code) => {
+				if (code != 0)
+					this.error(`${scriptName} exited with status ${code}.`);
+				
+				resolve(outputName);
+			});
+		});
 	}
 
 	run() {
--- test_config/validateFilename.py
+++ test_config/validateFilename.py
@@ -13,17 +13,13 @@ sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
 chunks = ''
 try:
-	print >> sys.stdout, "Python connecting to " + socketAddr
 	sock.connect(socketAddr)
-	print >> sys.stdout, "Python connected to " + socketAddr
 	while True:
 		chunk = sock.recv(1024)
-		print >> sys.stdout, "Python received " + chunk
 		if chunk == '':
 			break
-		chunks.append(chunk)
+		chunks += chunk
 
-	print >> sys.stderr, chunks
 except socket.error, msg:
 	print >>sys.stderr, msg
 	sys.exit(1)
@@ -31,3 +27,4 @@ except socket.error, msg:
 config = json.loads(chunks)
 
 copyfile(sys.argv[1], sys.argv[2])
+
