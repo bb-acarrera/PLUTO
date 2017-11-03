aad46241db28454d5156e568d702b0f983c1a99e
Paul Tokarchuk <ptokarchuk@uncharted.software>
2017-11-03 15:08:20 -0400

    Most of the work to get the validate filename python script working.

--- src/rules/validateFilename.py
+++ src/rules/validateFilename.py
@@ -1,10 +1,26 @@
 from __future__ import print_function
-
+import os.path
+import re
 import imp
 api = imp.load_source('PythonAPI', 'api/PythonAPI.py')
 
 class ValidateFilename(api.PythonAPIRule):
 	def __init__(self, config):
 		super(ValidateFilename, self).__init__(config)
+	
+	def run(self, inputFile, outputFile, encoding):
+		if not "regex" in self.config:
+			self.error("No regex specified.")
+		elif not "importConfig" in self.config:
+			self.error("No importConfig specified in the rule config.")
+		elif not "file" in self.config.importConfig:
+			self.error("No file specified in the rule config.importConfig.")
+		else:
+			filename = os.path.basename(self.config.importConfig.file)
+			prog = re.compile(self.config.regex)
+			if not prog.match(filename):
+				self.error(filename + " does not match the regular expression " + self.config.regex)
+			
+		super.run(inputFile, outputFile, encoding)
 		
 api.process(ValidateFilename)
