977ca0c6e0791974d515f0d5c7fb570083efd447
Paul Tokarchuk <ptokarchuk@uncharted.software>
2017-11-16 07:09:06 -0500

    Disable the "Run" button while processing.

--- src/client/app/helpers/if-in.js
+++ src/client/app/helpers/if-in.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
-export function ifIn([obj, list]) {
-  return list.indexOf(obj) > -1;
+export function ifIn([obj, list, ...rest]) {
+	if (rest == null || rest.length == 0)
+		return list.indexOf(obj) > -1;
+	else if (rest.length >= 2)
+		return list.indexOf(obj) > -1 ? rest[0] : rest[1];
+	else
+		return list.indexOf(obj) > -1 ? rest[0] : "";
 }
 
 export default Ember.Helper.helper(ifIn);
