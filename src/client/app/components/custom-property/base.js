6f682d2de7a6ef0189e71e897f30b719222c5fc2
Paul Tokarchuk <ptokarchuk@uncharted.software>
2017-12-13 07:46:07 -0500

    First attempt at using changeset validations.

--- src/client/app/components/custom-property/base.js
+++ src/client/app/components/custom-property/base.js
@@ -1,6 +1,42 @@
 import Ember from 'ember';
+import Changeset from 'ember-changeset';
+import * as Validators from 'ember-changeset-validations/validators';
+import lookupValidator from 'ember-changeset-validations';
 
 export default Ember.Component.extend({
+	init() {
+		this._super(...arguments);
+		const validations = this.get('uiItem.validations');
+		if (validations && validations instanceof Array) {
+			const itemName = this.get('uiItem.name');
+			const config = this.get('config');
+			var validators = [];
+			for (var i = 0; i < validations.length; i++) {
+				const validation = validations[i];
+				for (var key in validation) {
+					// Really expect only one key, but can't hurt to check all.
+					const fnName = "validate" + key.charAt(0).toUpperCase() + key.slice(1);
+					var validator = Validators[fnName];
+					if (validator) {
+						var validationArgs;
+						if (validation.get)
+							validationArgs = validation.get(key);
+						else
+							validationArgs = validation[key];
+	
+							validators.push(validator(validationArgs));
+					}
+					else {
+						console.error(`${fnName} is not a valid validator function.`);
+					}
+				}
+			}
+			const compValidator = {};
+			compValidator[itemName] = validators;
+
+			this.changeset = new Changeset(config, lookupValidator(compValidator), compValidator);
+		}
+	},
 	tagName: "",
 	valueChanged: Ember.observer('value', function() {
 		const config = this.get('config');
