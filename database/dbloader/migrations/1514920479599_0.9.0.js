fef83476abd525b909850e0879fb3e9cfdaa8a4c
Luke Corrigall <lcorrigall@oculusinfo.com>
2018-01-03 14:19:29 -0500

    Fixed the constraint and added FK for ruleset_id on runs

--- database/dbloader/migrations/1514920479599_0.9.0.js
+++ database/dbloader/migrations/1514920479599_0.9.0.js
@@ -5,7 +5,11 @@ exports.up = (pgm) => {
 	pgm.sql('CREATE EXTENSION IF NOT EXISTS btree_gist;');
 
 	pgm.addConstraint('rulesets', 'ruleset_targetFile_ruleset_id',
-		'EXCLUDE USING gist (target_file WITH =, ruleset_id WITH <>) WHERE (target_file IS NOT NULL)'
+		'EXCLUDE USING gist (target_file WITH =, ruleset_id WITH <>) WHERE (target_file IS NOT NULL AND deleted = FALSE)'
+	);
+
+	pgm.addConstraint('runs', 'FK_ruleset_id_ruleset_id',
+		'FOREIGN KEY (ruleset_id) REFERENCES rulesets (id) ON DELETE RESTRICT NOT VALID'
 	);
 };
 
@@ -13,5 +17,9 @@ exports.down = (pgm) => {
 
 	pgm.dropConstraint('rulesets', 'ruleset_targetFile_ruleset_id', {ifExists: true});
 
-	pgm.addConstraint('rulesets', 'ruleset_sourceFile_version', 'UNIQUE (target_file, version)');
+	pgm.dropConstraint('rulesets', 'FK_ruleset_id_ruleset_id', {ifExists: true});
+
+	// this was a bad constraint and downgrading may be impossible, so going to not include but leave
+	//   here as a reference to why
+	//pgm.addConstraint('rulesets', 'ruleset_sourceFile_version', 'UNIQUE (target_file, version)');
 };
