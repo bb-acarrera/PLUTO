8a3f861703b02c56c8433d220ad363cec426d1a9
Luke Corrigall <lcorrigall@oculusinfo.com>
2018-01-08 12:26:53 -0500

    Fixed bug with editing custom rules

--- src/client/app/routes/edit-configured-rule.js
+++ src/client/app/routes/edit-configured-rule.js
@@ -1,12 +1,16 @@
 import Ember from 'ember';
 import RSVP from 'rsvp';
+import RulesetEmberizer from '../mixins/ruleset-emberizer';
 
-export default Ember.Route.extend( {
+export default Ember.Route.extend(RulesetEmberizer, {
 	poll: Ember.inject.service(),
 	model ( params ) {
 
 		return RSVP.hash( {
-			rule: this.store.queryRecord( 'configuredrule', {id:params.rule_id} ),
+			rule: this.store.queryRecord( 'configuredrule', {id:params.rule_id}).then((result) => {
+				this.emberizeConfiguredRule(result);
+				return result;
+			},(err) => { throw err; } ),
 			importers: this.store.findAll( 'importer' ),
 			exporters: this.store.findAll( 'exporter' ),
 			defaultTargets: this.store.query('configuredrule', {
