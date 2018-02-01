752957cffdcf6b4f127aae4418dc598c8da8fa1d
Luke Corrigall <lcorrigall@oculusinfo.com>
2018-02-01 14:38:02 -0500

    Using docker container to run tests now

--- build/bin/runUnitTests.sh
+++ build/bin/runUnitTests.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
-PATH=$PWD/node_modules/qunitjs/bin:$PATH
-qunit 'src/server/tests/*/test-*.js' 'src/validator/tests/*/test-*.js'
-exit $?
+
+ROOT=`dirname $0`/../..
+ROOT=`(cd $ROOT; echo $PWD)`
+
+docker build -t pluto_base -f $ROOT/dev_container/plutoBase.Dockerfile $ROOT/dev_container
+
+cp $ROOT/package.json $ROOT/build
+
+docker build -t pluto_test -f $ROOT/build/test.Dockerfile $ROOT/build
+
+docker run -v $ROOT:/code --net=plutonet --rm -a stdout -a stderr pluto_test
+
+#PATH=$PWD/node_modules/qunitjs/bin:$PATH
+#qunit 'src/server/tests/*/test-*.js' 'src/validator/tests/*/test-*.js'
+#exit $?
---
+++ build/test.Dockerfile
@@ -0,0 +1,13 @@
+FROM pluto_base
+
+WORKDIR /code
+
+COPY package.json /code/package.json
+RUN npm install
+RUN mv /code/node_modules /node_modules
+
+VOLUME ["/code"]
+ENV PLUTOAPI /code/src/api
+ENV PLUTOCONFIG /code/src/runtime
+
+CMD ["/node_modules/qunitjs/bin/qunit", "src/server/tests/*/test-*.js", "src/validator/tests/*/test-*.js"]
