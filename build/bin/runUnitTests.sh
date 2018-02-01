5f4ce1a27850aabfd011f4a5275755a01d7d9fb5
Luke Corrigall <lcorrigall@oculusinfo.com>
2018-02-01 14:40:15 -0500

    cleanup and added script with old test method

--- build/bin/runUnitTests.sh
+++ build/bin/runUnitTests.sh
@@ -10,7 +10,3 @@ cp $ROOT/package.json $ROOT/build
 docker build -t pluto_test -f $ROOT/build/test.Dockerfile $ROOT/build
 
 docker run -v $ROOT:/code --net=plutonet --rm -a stdout -a stderr pluto_test
-
-#PATH=$PWD/node_modules/qunitjs/bin:$PATH
-#qunit 'src/server/tests/*/test-*.js' 'src/validator/tests/*/test-*.js'
-#exit $?
