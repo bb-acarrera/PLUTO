821273a48ae5fddbacecfd7b304c0a9ca4d314f9
Luke Corrigall <lcorrigall@oculusinfo.com>
2018-02-01 16:12:48 -0500

    start using .dockeringnore files

--- build/bin/runUnitTests.sh
+++ build/bin/runUnitTests.sh
@@ -5,8 +5,6 @@ ROOT=`(cd $ROOT; echo $PWD)`
 
 docker build -t pluto_base -f $ROOT/dev_container/plutoBase.Dockerfile $ROOT/dev_container
 
-cp $ROOT/package.json $ROOT/build
-
-docker build -t pluto_test -f $ROOT/build/test.Dockerfile $ROOT/build
+docker build -t pluto_test -f $ROOT/build/test.Dockerfile $ROOT
 
 docker run -v $ROOT:/code --net=plutonet --rm -a stdout -a stderr pluto_test
