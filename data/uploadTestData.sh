b9d95b9ad50e4a71f561f6e44458d1e91f072a7b
Luke Corrigall <lcorrigall@oculusinfo.com>
2018-03-05 17:24:46 -0500

    updated data upload

--- data/uploadTestData.sh
+++ data/uploadTestData.sh
@@ -2,8 +2,8 @@
 
 echo Run from the data folder
 
-node s3.js -b test -n
-node s3.js -b testoutput -n
+node s3.js -c testConfig.json -b test -n
+node s3.js -c testConfig.json -b testoutput -n
 
-node s3.js -b test -r worldcities.csv -l ../src/examples/data/worldcities.csv -u
-node s3.js -b test -r simplemaps-worldcities-basic.csv -l ../src/examples/data/simplemaps-worldcities-basic.csv -u
+node s3.js -c testConfig.json -b test -r worldcities.csv -l ../src/examples/data/worldcities.csv -u
+node s3.js -c testConfig.json -b test -r simplemaps-worldcities-basic.csv -l ../src/examples/data/simplemaps-worldcities-basic.csv -u
