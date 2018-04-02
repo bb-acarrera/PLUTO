b9a5a9f7b0567048318e76d7a1b4da1f88fc16a5
Luke Corrigall <lcorrigall@oculusinfo.com>
2018-04-02 11:19:51 -0400

    Added validations for base config #216

--- src/client/app/models/rulesetconfigui.js
+++ src/client/app/models/rulesetconfigui.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
+import RuleValidatorGenerator from '../mixins/rule-validator-generator';
 
 export default DS.Model.extend({
   filename : DS.attr('string'),
   ui: DS.attr()
-});
+}, RuleValidatorGenerator);
