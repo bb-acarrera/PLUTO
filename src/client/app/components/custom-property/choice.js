7d211b29284fd15e1a0a63f0afede5eb32b92131
Luke Corrigall <lcorrigall@oculusinfo.com>
2018-04-24 16:21:57 -0400

    Changed choice and column to use power select instead of basic html select

--- src/client/app/components/custom-property/choice.js
+++ src/client/app/components/custom-property/choice.js
@@ -1,4 +1,55 @@
 import Base from './base';
+import Ember from 'ember';
 
 export default Base.extend({
+
+    choices: Ember.computed('uiItem.choices', function() {
+
+        let choices = [];
+        let items = this.get('uiItem.choices');
+        
+        if(items) {
+            items.forEach(element => {
+                if(Array.isArray(element)) {
+                    choices.push({label: element[1], value : element[0]});
+                } else if(element.label) {
+                    choices.push(element);
+                } else {
+                    choices.push({label: element, value : element});
+                }
+            });
+        }
+
+        return choices;
+
+    }),
+    
+    selectedValue: Ember.computed('choices', 'value', function() {
+
+        let choices = this.get('choices');
+        let value = this.get('value');
+        let item = null;
+        
+        if(choices) {
+            choices.forEach(element => {
+                if(element.value === value) {
+                    item = element;
+                }
+            });
+        }
+
+        return item;
+
+    }),
+
+    actions: {
+        setValue(entry) {
+
+            if(!entry) {
+                this.set('value', null);
+            } else {
+                this.set('value', entry.value);
+            }
+		}
+    }
 });
