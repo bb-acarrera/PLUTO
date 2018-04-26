bcafc811a893363f267b74e94feceed1ee0e1e7a
Luke Corrigall <lcorrigall@oculusinfo.com>
2018-04-26 16:45:09 -0400

    added rabbitMQ sample files

---
+++ rabbitMQ/startRabbit.sh
@@ -0,0 +1,8 @@
+# /bin/sh
+
+docker stop rabbitTest
+docker rm rabbitTest
+
+docker volume create testRabbitData
+
+docker run -v testRabbitData:/var/lib/rabbitmq -p 4369:4369 -p 5672:5672 -p 15672:15672 -p 25672:25672 -d --name rabbitTest rabbitmq:3.7.4-alpine
