599c109bc3879c9c2cdc59cfea6e8b4fbde27baa
asumila <alexei.sumila@gmail.com>
2018-04-30 14:51:09 +0200

    null check on properties override

--- src/server/routes/rules.js
+++ src/server/routes/rules.js
@@ -29,7 +29,7 @@ class RulesRouter extends BaseRouter {
 
 		let parsers = this.rulesLoader.rules;
 		parsers.forEach((val)=>{
-			if(this.config.validatorConfig.prepertiesOverride.rules[val.id]) {
+			if(this.config.validatorConfig.prepertiesOverride && this.config.validatorConfig.prepertiesOverride.rules[val.id]) {
 				val.attributes.ui.properties.forEach( ( property ) => {
 					let config = this.config.validatorConfig.prepertiesOverride.rules[val.id][property.name];
 					if ( config ){
@@ -50,7 +50,7 @@ class RulesRouter extends BaseRouter {
 
         let parsers = this.rulesLoader.parsers;
 		parsers.forEach((val)=>{
-		    if(this.config.validatorConfig.prepertiesOverride.parsers[val.id]) {
+		    if(this.config.validatorConfig.prepertiesOverride && this.config.validatorConfig.prepertiesOverride.parsers[val.id]) {
 				val.attributes.ui.properties.forEach( ( property ) => {
 				    let config = this.config.validatorConfig.prepertiesOverride.parsers[val.id][property.name];
 					if ( config ){
@@ -85,7 +85,7 @@ class RulesRouter extends BaseRouter {
 
 		let properties = this.rulesetConfig[0].attributes.ui.properties;
 		properties.forEach((val)=>{
-			if(this.config.validatorConfig.prepertiesOverride.globalconfig[val.name]) {
+			if(this.config.validatorConfig.prepertiesOverride && this.config.validatorConfig.prepertiesOverride.globalconfig[val.name]) {
                 let config = this.config.validatorConfig.prepertiesOverride.globalconfig[val.name];
                 if ( config ){
                     val.default = config.default;
@@ -112,7 +112,7 @@ class RulesRouter extends BaseRouter {
 
 		let properties = this.rulesLoader.posttasks[0].attributes.ui.properties;
 		properties.forEach((val)=>{
-			if(this.config.validatorConfig.prepertiesOverride.posttasks[val.name]) {
+			if(this.config.validatorConfig.prepertiesOverride && this.config.validatorConfig.prepertiesOverride.posttasks[val.name]) {
 				let config = this.config.validatorConfig.prepertiesOverride.posttasks[val.name];
 				if ( config ){
 					val.default = config.default;
