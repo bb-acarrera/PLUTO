c73c9086f91b7fc7432380621fd2b561ed97b2a4
Andrew Carrera <acarrera2@bloomberg.net>
2018-06-08 11:19:59 -0400

    Add debug information for debugging circleci

--- build/bin/runUnitTestsCI.sh
+++ build/bin/runUnitTestsCI.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
 ROOT=`dirname $0`/../..
+echo $ROOT
 ROOT=`(cd $ROOT; echo $PWD)`
+echo $ROOT
+
 
 docker build --rm=false -t pluto_base -f $ROOT/dev_container/plutoBase.Dockerfile $ROOT/dev_container
 
 docker build --rm=false -t pluto_dev -f $ROOT/dev_container/dev.Dockerfile $ROOT
 
 docker build --rm=false -t pluto_test -f $ROOT/build/test.Dockerfile $ROOT
+
+ls -l $ROOT
 
 docker run -v $ROOT:/code -v /code/node_modules --rm -a stdout -a stderr pluto_test
