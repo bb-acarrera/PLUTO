c2f81071fbbd1e02f9caaff94c76cc48c404ded7
Andrew Carrera <acarrera2@bloomberg.net>
2018-06-11 08:43:05 -0400

    Remove debug statements

--- build/bin/runUnitTestsCI.sh
+++ build/bin/runUnitTestsCI.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
 
 ROOT=`dirname $0`/../..
-echo $ROOT
 ROOT=`(cd $ROOT; echo $PWD)`
-echo $ROOT
-
 
 docker build --rm=false -t pluto_base -f $ROOT/dev_container/plutoBase.Dockerfile $ROOT/dev_container
 
 docker build --rm=false -t pluto_dev -f $ROOT/dev_container/dev.Dockerfile $ROOT
 
 docker build --rm=false -t pluto_test -f $ROOT/build/test.Dockerfile $ROOT
-
-ls -l $ROOT
-ls -l $ROOT/src/server/tests/*/test-*.js
-ls -l $ROOT/src/validator/tests/*/test-*.js
 
 docker run -v $ROOT:/code -v /code/node_modules --rm -a stdout -a stderr pluto_test
